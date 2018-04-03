8a1e0a788fa243f6e4c433d40454b83b6f3daf26
Wenzel, Toni <toni.wenzel@googlemail.com>
2018-04-03 10:02:24 +0200

    Get assembly from startup class

--- src/Microsoft.Extensions.Logging.Log4Net.AspNetCore/Log4NetProvider.cs
+++ src/Microsoft.Extensions.Logging.Log4Net.AspNetCore/Log4NetProvider.cs
@@ -1,140 +1,166 @@
 ﻿namespace Microsoft.Extensions.Logging
 {
-	using System;
-	using System.Collections.Concurrent;
-	using System.IO;
-	using System.Text;
-	using System.Xml;
-	using System.Reflection;
+    using System;
+    using System.Collections.Concurrent;
+    using System.IO;
+    using System.Text;
+    using System.Xml;
+    using System.Reflection;
 
-	using log4net;
-	using log4net.Config;
-	using log4net.Repository;
+    using log4net;
+    using log4net.Config;
+    using log4net.Repository;
 
-	/// <summary>
-	/// The log4net provider class.
-	/// </summary>
-	public class Log4NetProvider : ILoggerProvider
-	{
-		/// <summary>
-		/// The log4net repository.
-		/// </summary>
-		private ILoggerRepository loggerRepository;
+    /// <summary>
+    /// The log4net provider class.
+    /// </summary>
+    public class Log4NetProvider : ILoggerProvider
+    {
+        /// <summary>
+        /// The log4net repository.
+        /// </summary>
+        private ILoggerRepository loggerRepository;
 
-		/// <summary>
-		/// The exception formatter Func.
-		/// </summary>
-		private Func<object, Exception, string> exceptionFormatter;
+        /// <summary>
+        /// The exception formatter Func.
+        /// </summary>
+        private Func<object, Exception, string> exceptionFormatter;
 
-		/// <summary>
-		/// The loggers collection.
-		/// </summary>
-		private readonly ConcurrentDictionary<string, Log4NetLogger> loggers = new ConcurrentDictionary<string, Log4NetLogger>();
+        /// <summary>
+        /// The loggers collection.
+        /// </summary>
+        private readonly ConcurrentDictionary<string, Log4NetLogger> loggers = new ConcurrentDictionary<string, Log4NetLogger>();
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Log4NetProvider"/> class.
-		/// </summary>
-		/// <param name="log4NetConfigFile">The log4 net configuration file.</param>
-		public Log4NetProvider(string log4NetConfigFile)
-			: this(log4NetConfigFile, null)
-		{
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Log4NetProvider"/> class.
+        /// </summary>
+        /// <param name="log4NetConfigFile">The log4 net configuration file.</param>
+        public Log4NetProvider(string log4NetConfigFile)
+            : this(log4NetConfigFile, null)
+        {
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Log4NetProvider"/> class.
-		/// </summary>
-		/// <param name="log4NetConfigFile">The log4NetConfigFile.</param>
-		public Log4NetProvider(string log4NetConfigFile, Func<object, Exception, string> exceptionFormatter)
-		{
-			this.exceptionFormatter = exceptionFormatter ?? FormatExceptionByDefault;
-			loggerRepository = LogManager.CreateRepository(Assembly.GetEntryAssembly(),
-														   typeof(log4net.Repository.Hierarchy.Hierarchy));
-			XmlConfigurator.Configure(loggerRepository, Parselog4NetConfigFile(log4NetConfigFile));
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Log4NetProvider"/> class.
+        /// </summary>
+        /// <param name="log4NetConfigFile">The log4NetConfigFile.</param>
+        public Log4NetProvider(string log4NetConfigFile, Func<object, Exception, string> exceptionFormatter)
+        {
+            this.exceptionFormatter = exceptionFormatter ?? FormatExceptionByDefault;
+            loggerRepository = LogManager.CreateRepository(Assembly.GetEntryAssembly() ?? GetCallingAssemblyFromStartup(),
+                                                           typeof(log4net.Repository.Hierarchy.Hierarchy));
+            XmlConfigurator.Configure(loggerRepository, Parselog4NetConfigFile(log4NetConfigFile));
+        }
 
-		/// <summary>
-		/// Creates the logger.
-		/// </summary>
-		/// <param name="categoryName">The category name.</param>
-		/// <returns>The <see cref="ILogger"/> instance.</returns>
-		public ILogger CreateLogger(string categoryName)
-		{
-			return this.loggers.GetOrAdd(categoryName, this.CreateLoggerImplementation);
-		}
+        /// <summary>
+        /// Creates the logger.
+        /// </summary>
+        /// <param name="categoryName">The category name.</param>
+        /// <returns>The <see cref="ILogger"/> instance.</returns>
+        public ILogger CreateLogger(string categoryName)
+        {
+            return this.loggers.GetOrAdd(categoryName, this.CreateLoggerImplementation);
+        }
 
-		/// <summary>
-		/// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-		/// </summary>
-		public void Dispose()
-		{
-			this.Dispose(true);
-			GC.SuppressFinalize(this);
-		}
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
-		/// <summary>
-		/// Releases unmanaged and - optionally - managed resources.
-		/// </summary>
-		/// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
-		protected virtual void Dispose(bool disposing)
-		{
-			if (disposing)
-			{
-			}
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources.
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
 
-			this.loggers.Clear();
-		}
+            this.loggers.Clear();
+        }
 
-		/// <summary>
-		/// Parses log4net config file.
-		/// </summary>
-		/// <param name="filename">The filename.</param>
-		/// <returns>The <see cref="XmlElement"/> with the log4net XML element.</returns>
-		private static XmlElement Parselog4NetConfigFile(string filename)
-		{
-			using (FileStream fp = File.OpenRead(filename))
-			{
-				var settings = new XmlReaderSettings
-				{
-					DtdProcessing = DtdProcessing.Prohibit
-				};
+        /// <summary>
+        /// Parses log4net config file.
+        /// </summary>
+        /// <param name="filename">The filename.</param>
+        /// <returns>The <see cref="XmlElement"/> with the log4net XML element.</returns>
+        private static XmlElement Parselog4NetConfigFile(string filename)
+        {
+            using (FileStream fp = File.OpenRead(filename))
+            {
+                var settings = new XmlReaderSettings
+                {
+                    DtdProcessing = DtdProcessing.Prohibit
+                };
 
-				var log4netConfig = new XmlDocument();
-				using (var reader = XmlReader.Create(fp, settings))
-				{
-					log4netConfig.Load(reader);
-				}
+                var log4netConfig = new XmlDocument();
+                using (var reader = XmlReader.Create(fp, settings))
+                {
+                    log4netConfig.Load(reader);
+                }
 
-				return log4netConfig["log4net"];
-			}
-		}
+                return log4netConfig["log4net"];
+            }
+        }
 
-		/// <summary>
-		/// Creates the logger implementation.
-		/// </summary>
-		/// <param name="name">The name.</param>
-		/// <returns>The <see cref="Log4NetLogger"/> instance.</returns>
-		private Log4NetLogger CreateLoggerImplementation(string name)
-			=> new Log4NetLogger(loggerRepository.Name, name)
-					.UsingCustomExceptionFormatter(this.exceptionFormatter);
+        /// <summary>
+        /// Creates the logger implementation.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>The <see cref="Log4NetLogger"/> instance.</returns>
+        private Log4NetLogger CreateLoggerImplementation(string name)
+            => new Log4NetLogger(loggerRepository.Name, name)
+                    .UsingCustomExceptionFormatter(this.exceptionFormatter);
 
-		/// <summary>
-		/// Formats an exception by default.
-		/// </summary>
-		/// <typeparam name="TState">The type of the state.</typeparam>
-		/// <param name="state">The state of the logged object.</param>
-		/// <param name="exception">The exception to be logged.</param>
-		/// <returns>The text with the formatted message.</returns>
-		private static string FormatExceptionByDefault<TState>(TState state, Exception exception)
-		{
-			var builder = new StringBuilder();
-			builder.Append(state.ToString());
-			builder.Append(" - ");
-			if (exception != null)
-			{
-				builder.Append(exception.ToString());
-			}
+        /// <summary>
+        /// Formats an exception by default.
+        /// </summary>
+        /// <typeparam name="TState">The type of the state.</typeparam>
+        /// <param name="state">The state of the logged object.</param>
+        /// <param name="exception">The exception to be logged.</param>
+        /// <returns>The text with the formatted message.</returns>
+        private static string FormatExceptionByDefault<TState>(TState state, Exception exception)
+        {
+            var builder = new StringBuilder();
+            builder.Append(state.ToString());
+            builder.Append(" - ");
+            if (exception != null)
+            {
+                builder.Append(exception.ToString());
+            }
 
             return builder.ToString();
+        }
+
+        /// <summary>
+        /// Tries to retrieve the assembly from a "Startup" type found in the stacktrace.
+        /// </summary>
+        /// <returns>Null for NetCoreApp 1.1 otherwise Assembly of Startup type if found in stacktrace.</returns>
+        private static Assembly GetCallingAssemblyFromStartup()
+        {
+#if NETCOREAPP1_1
+            return null;
+#else
+            var stackTrace = new System.Diagnostics.StackTrace(2);
+
+            for (int i = 0; i < stackTrace.FrameCount; i++)
+            {
+                var frame = stackTrace.GetFrame(i);
+                var type = frame.GetMethod()?.DeclaringType;
+
+                if (string.Equals(type?.Name, "Startup", StringComparison.OrdinalIgnoreCase))
+                {
+                    return type.Assembly;
+                }
+            }
+
+            return null;
+#endif
         }
     }
 }
