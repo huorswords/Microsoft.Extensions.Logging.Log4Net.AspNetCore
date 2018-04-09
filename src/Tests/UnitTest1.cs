f04db81195e43b5f64d8914a1ebbca275c3f37bc
Angel G <angel.g-santos@outlook.com>
2018-04-09 09:50:30 +0200

    Include test to check stacktrace layout

--- src/Tests/UnitTest1.cs
+++ src/Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Tests
 {
@@ -16,5 +17,28 @@ namespace Tests
 
 			Assert.Inconclusive();
         }
+
+		[TestMethod]
+		public void UsePatternLayoutOnExceptions()
+		{
+			var provider = new Log4NetProvider("log4net.config", null);
+			var logger = provider.CreateLogger("Test");
+
+			try
+			{
+				ThrowException();
+			}
+			catch (Exception ex)
+			{
+				logger.LogCritical(10, ex, "Catched message");
+			}
+
+			Assert.Inconclusive();
+		}
+
+		private void ThrowException()
+		{
+			throw new InvalidOperationException("A message");
+		}
     }
 }
