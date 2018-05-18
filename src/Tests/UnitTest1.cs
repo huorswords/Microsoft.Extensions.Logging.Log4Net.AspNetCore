b8c13b6a07885ee8e994a3e209dc4cfb741e81c6
Chan Jun Wei <junwei.chan@numtech.com>
2018-05-18 12:37:16 +0800

    Support for configuration watching #24

--- src/Tests/UnitTest1.cs
+++ src/Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
 
 namespace Tests
 {
@@ -41,5 +42,72 @@ namespace Tests
         /// </summary>
         /// <exception cref="InvalidOperationException">A message</exception>
         private static void ThrowException() => throw new InvalidOperationException("A message");
+
+        private const string Log4NetConfigFileName = "log4net.config";
+
+        [TestMethod]
+        public void LogFileExistsWithFileName0()
+        {
+            const string fileName = "0.log";
+            //Arrange
+            DeleteIfExist(fileName);
+            var logFilePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            var provider = new Log4NetProvider(Log4NetConfigFileName, new Log4NetParams { FileName = fileName });
+            var logger = provider.CreateLogger("Test");
+
+            logger.LogCritical("A message");
+
+            Assert.IsTrue(File.Exists(logFilePath));
+            provider.Dispose();
+        }
+
+        [TestMethod]
+        public void LogFileExistsWithFileName1()
+        {
+            const string fileName = "1.log";
+
+            //Arrange
+            DeleteIfExist(fileName);
+            var logFilePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            var provider = new Log4NetProvider(Log4NetConfigFileName, new Log4NetParams { FileName = fileName });
+            var logger = provider.CreateLogger("Test");
+
+            logger.LogCritical("A message");
+
+            Assert.IsTrue(File.Exists(logFilePath));
+            provider.Dispose();
+        }
+
+        [TestMethod]
+        public void LogFileExistsWithFileName2_Watch()
+        {
+            const string fileName = "2.log";
+            const string watchConfigName = "watch-" + Log4NetConfigFileName;
+
+            //Arrange
+            DeleteIfExist(watchConfigName);
+            DeleteIfExist(fileName);
+            var logFilePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            var provider = new Log4NetProvider(Log4NetConfigFileName, new Log4NetParams { FileName = fileName, Watch = true });
+            var logger = provider.CreateLogger("Test");
+
+            logger.LogCritical("A message");
+
+            Assert.IsTrue(File.Exists(watchConfigName));
+            Assert.IsTrue(File.Exists(logFilePath));
+            provider.Dispose();
+        }
+
+        private static void DeleteIfExist(string fileName)
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
     }
 }
