b8f8a3563ae5ad79c42d03577765a3715cf5a30c
HuorSwords <angel.g-santos@outlook.com>
2018-06-05 21:01:16 +0200

    Add suggested extension to allow create ILogger with a given type name

--- src/Microsoft.Extensions.Logging.Log4Net.AspNetCore/Log4NetProvider.cs
+++ src/Microsoft.Extensions.Logging.Log4Net.AspNetCore/Log4NetProvider.cs
@@ -11,6 +11,33 @@
     using log4net.Repository;
 
     /// <summary>
+    /// Log4Net provider extensions.
+    /// </summary>
+    public static class Log4NetProviderExtensions
+    {
+        /// <summary>
+        /// Creates a logger with the name of the given <see cref="TName"/> type.
+        /// </summary>
+        /// <typeparam name="TName">The type of the class to be used as name of the logger.</typeparam>
+        /// <param name="self">An ILoggerProvider instance.</param>
+        /// <returns>An instance of the <see cref="ILogger"/>.</returns>
+        public static ILogger CreateLogger<TName>(this ILoggerProvider self) where TName : class
+        {
+            if (self == null)
+            {
+                throw new ArgumentNullException(nameof(self));
+            }
+            
+            if (!self.GetType().IsAssignableFrom(typeof(Log4NetProvider)))
+            {
+                throw new ArgumentOutOfRangeException(nameof(self), "The ILoggerProvider should be of type Log4NetProvider.");
+            }
+
+            return self.CreateLogger(typeof(TName).AssemblyQualifiedName);
+        }
+    }
+
+    /// <summary>
     /// The log4net provider class.
     /// </summary>
     public class Log4NetProvider : ILoggerProvider
---
+++ src/Tests/Log4NetProviderExtensionsShould.cs
@@ -0,0 +1,22 @@
+﻿namespace Tests
+{
+    using Microsoft.Extensions.Logging;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class Log4NetProviderExtensionsShould
+    {
+        /// <summary>
+        /// 
+        /// 
+        [Microsoft.VisualStudio.TestTools.UnitTesting.TestMethod]
+        public void MyTestMethod()
+        {
+            var provider = new Log4NetProvider("log4net.config");
+
+            var logger = provider.CreateLogger<Log4NetProviderExtensionsShould>();
+
+            Assert.IsNotNull(logger);
+        }
+    }
+}
