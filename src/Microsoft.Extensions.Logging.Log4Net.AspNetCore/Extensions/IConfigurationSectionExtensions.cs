5034ce74bbdcd0bec31acbde6fb7b1e273fe48a8
Angel Garcia <angel.g-santos@outlook.com>
2018-08-27 12:41:16 +0200

    Enable Log4NetProviderOptions configuration
    After the change, the developer will be able to configure the log4net
    provider within a class named Log4NetProviderOptions.
    
    This class is intented to allow the developer configure properties as:
    - The logger default name.
    - The logger repository name.
    - The Log4Net.config file name / path.
    - The log4net property override collection, to allow configure the
    log4net file values on start through appsettings.json or environment
    variables.
    - The watch flag to be watching the log4net config file.
    - The OverrideCriticalLevelWith, that allows:
      a) Critical - Sets the logger to use the native Critical level.
      b) Anything else - Sets the logger to use the Fatal level when
      critical is registered.
    
    This commit solves #44

--- src/Microsoft.Extensions.Logging.Log4Net.AspNetCore/Extensions/IConfigurationSectionExtensions.cs
+++ src/Microsoft.Extensions.Logging.Log4Net.AspNetCore/Extensions/IConfigurationSectionExtensions.cs
@@ -1,13 +1,15 @@
 ﻿namespace Microsoft.Extensions.Logging.Log4Net.AspNetCore.Extensions
 {
-    using System.Collections.Generic;
+	using System;
+	using System.Collections.Generic;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.Logging.Log4Net.AspNetCore.Entities;
 
-    /// <summary>
-    /// class containing extensions for IConfigurationSection interface.
-    /// </summary>
-    internal static class IConfigurationSectionExtensions
+	/// <summary>
+	/// class containing extensions for IConfigurationSection interface.
+	/// </summary>
+	[Obsolete("To be removed on next releases")]
+	internal static class IConfigurationSectionExtensions
     {
         /// <summary>
         /// Converts IConfigurationSection to dictionary.
@@ -15,7 +17,8 @@
         /// <param name="configurationSection">The configuration section.</param>
         /// <returns>The dictionary</returns>
         /// <exception cref="ArgumentNullException">Throws if <paramref name="configurationSection"/> is null.</exception>
-        public static IEnumerable<NodeInfo> ConvertToNodesInfo(this IConfigurationSection configurationSection) =>
+        [Obsolete("To be removed on next releases")]
+		public static IEnumerable<NodeInfo> ToNodesInfo(this IConfigurationSection configurationSection) =>
             configurationSection.Get<IEnumerable<NodeInfo>>();
     }
 }
