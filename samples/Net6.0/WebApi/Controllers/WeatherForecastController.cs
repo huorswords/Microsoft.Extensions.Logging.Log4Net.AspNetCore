7e0d8d735d535bb5bdbff5f3b25ba3270e610f74
Ángel García Santos <angel.g-santos@outlook.com>
2021-11-10 21:13:36 +0100

    fix: Make the controller to write different types of logging messages

--- samples/Net6.0/WebApi/Controllers/WeatherForecastController.cs
+++ samples/Net6.0/WebApi/Controllers/WeatherForecastController.cs
@@ -21,12 +21,15 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        WeatherForecast[] weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
             Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        }).ToArray();
+
+        _logger.LogCritical(new Exception("ExceptionText"), "This is {weather}", weatherForecasts.First().Summary);
+        _logger.LogTrace("Weather forecast ready!");
+        return weatherForecasts;
     }
 }
